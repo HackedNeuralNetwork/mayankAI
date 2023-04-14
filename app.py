1db3b6e84dc2ea97b7878eba88212eba65e28a4c
Hacked Neural Network <info.hackedneuralnetwork@gmail.com>
2023-04-14 14:48:46 +0530

    Free Version AI

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template,request,jsonify
-import sqlite3
-import db
 import requests
 
 
@@ -59,32 +57,11 @@ def predict_next_outcome():
     tie_count = last_10_results.count('Tie')
 
     if high_count > low_count and high_count > tie_count:
-        return "Low"
+        return "Low Card"
     elif low_count > high_count and low_count > tie_count:
-        return "High"
+        return "High Card"
     else:
-        return "Tie is High"
-
-# def calculate_weighted_probabilities():
-#     game_results = get_last10game()
-#     recent_games = game_results[:5]
-#     less_recent_games = game_results[5:]
-#
-#     recent_weight = 0.6
-#     less_recent_weight = 0.4
-#
-#     outcomes = ["High", "Low", "Tie"]
-#     counts = {outcome: [0, 0] for outcome in outcomes}
-#
-#     for outcome in outcomes:
-#         counts[outcome][0] = recent_games.count(outcome)
-#         counts[outcome][1] = less_recent_games.count(outcome)
-#
-#     weighted_probabilities = {
-#         outcome: (counts[outcome][0] * recent_weight + counts[outcome][1] * less_recent_weight) / 10
-#         for outcome in outcomes
-#     }
-#     return str(weighted_probabilities)
+        return "Tie is High Card"
 
 
 # Get Time and Game ID
@@ -92,119 +69,8 @@ def predict_next_outcome():
 def get_data():
     time, g_id = get_time_gID()
     last10 = get_last10game()
-    user_balance = db.get_balance()[0][0]
     nextGame = predict_next_outcome()
-    return jsonify({"time": time, "g_id": g_id, "last10": str(last10), "balance": user_balance, "nextGame": nextGame})
-
-
-
-# Result Page
-@app.route("/result")
-def result():
-    data = db.get_results()
-    c_ac,w_win = get_Accuracy()
-    return render_template("results.html",data=data,ac=c_ac,w_in=w_win)
-
-def get_Accuracy():
-    global total_win,total_loss
-    total_win = 0
-    total_loss = 0
-    getwin = db.get_win()
-    for i in getwin:
-        if i[0] == "Y":
-            total_win += 1
-        elif i[0] =="N":
-            total_loss += 1
-
-    if total_win > total_loss:
-        ac = total_win/len(getwin)*100
-        return ac,"win"
-    elif total_win == total_loss:
-        return 50,"win"
-    else:
-        ac = total_loss/len(getwin)*100
-        return ac,"Loss"
-
-# @app.route('/acc')
-# def get
-
-@app.route('/bet',methods=['POST'])
-def update_balance_db():
-    balance = db.get_balance()[0][0]
-    balance -= 100
-    conn = sqlite3.connect("ai.db")
-    c = conn.cursor()
-    c.execute("UPDATE lucky7a SET bal = ? WHERE id = (SELECT id FROM lucky7a ORDER BY id DESC LIMIT 1)", (balance,))
-    conn.commit()
-    conn.close()
-    return render_template("lucky7a.html")
-
-@app.route('/win',methods=['POST'])
-def win_balance_db():
-    balance = db.get_balance()[0][0]
-    balance += 200
-    conn = sqlite3.connect("ai.db")
-    c = conn.cursor()
-    c.execute("UPDATE lucky7a SET bal = ? WHERE id = (SELECT id FROM lucky7a ORDER BY id DESC LIMIT 1)", (balance,))
-    conn.commit()
-    conn.close()
-    return render_template("lucky7a.html")
-
-@app.route('/create_db', methods=['POST'])
-def create_db():
-    conn = sqlite3.connect("ai.db")
-    c = conn.cursor()
-    c.execute(""" CREATE TABLE lucky7a (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        guess_game TEXT,
-        game_result TEXT,
-        bal INTEGER,
-        win TEXT)
-        """)
-    conn.commit()
-    conn.close()
-    return render_template("index.html")
-
-@app.route('/insert_data', methods=['POST'])
-def insert_data():
-    guess_game = request.form['guess_game']
-    game_result = request.form['game_result']
-    bal = int(request.form['bal'])
-    win = request.form['win']
-    conn = sqlite3.connect("ai.db")
-    c = conn.cursor()
-    try:
-        c.execute("INSERT INTO lucky7a (guess_game,game_result,bal,win) VALUES ('{}','{}',{},'{}')".format(guess_game, game_result, bal, win))
-    except:
-        pass
-    conn.commit()
-    conn.close()
-    return render_template("index.html")
-
-@app.route('/delete_db', methods=['POST'])
-def delete_db():
-    conn = sqlite3.connect("ai.db")
-    c = conn.cursor()
-    try:
-        c.execute(""" DROP TABLE lucky7a """)
-        c.execute(""" CREATE TABLE lucky7a (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                guess_game TEXT,
-                game_result TEXT,
-                bal INTEGER,
-                win TEXT)
-                """)
-    except:
-        c.execute(""" CREATE TABLE lucky7a (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                guess_game TEXT,
-                game_result TEXT,
-                bal INTEGER,
-                win TEXT)
-                """)
-    conn.commit()
-    conn.close()
-    return render_template("index.html")
+    return jsonify({"time": time, "g_id": g_id, "last10": str(last10), "nextGame": nextGame})
 
 if __name__ == "__main__":
     app.run(debug=True)
